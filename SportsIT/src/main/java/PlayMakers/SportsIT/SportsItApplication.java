050dafc27d661608d6c9368925838294c54e76fd
MyeongQ <gmk0904@ajou.ac.kr>
2023-03-03 15:36:36 +0900

    [Modify] Group 이름 수정

--- SportsIT/src/main/java/PlayMakers/SportsIT/SportsItApplication.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/SportsItApplication.java
@@ -1,4 +1,4 @@
-package TeamMakers.SportsIT;
+package PlayMakers.SportsIT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- SportsIT/src/test/java/PlayMakers/SportsIT/SportsItApplicationTests.java
+++ SportsIT/src/test/java/PlayMakers/SportsIT/SportsItApplicationTests.java
@@ -1,4 +1,4 @@
-package TeamMakers.SportsIT;
+package PlayMakers.SportsIT;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
