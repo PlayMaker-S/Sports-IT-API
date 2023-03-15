d71fa9cfd0e6863752e7de01b0857c57085e24d2
MyeongQ <gmk0904@ajou.ac.kr>
2023-03-15 20:43:53 +0900

    [Feat] Spring Security 설정 파일 추가, CORS Filter 설정

---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/auth/security/CorsFilter.java
@@ -0,0 +1,41 @@
+package PlayMakers.SportsIT.auth.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With, Authorization");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- SportsIT/src/main/java/PlayMakers/SportsIT/auth/security/PrincipalUser.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/auth/security/PrincipalUser.java
@@ -12,25 +12,29 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Slf4j
-public class PrincipalDetails implements UserDetails {
+public class PrincipalUser extends User {
     private Member member;
 
-    public PrincipalDetails(Member member) {
-        this.member = member;
+    //public PrincipalDetails(Member member) {
+    //    this.member = member;
+    //}
+
+    public PrincipalUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public PrincipalUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
     }
 
     // 해당 User의 권한을 반환
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collect = new ArrayList<>();
-        collect.add(new GrantedAuthority() {
-            @Override  // 권한(MemberType)을 리턴
-            public String getAuthority() {
-                return member.getMemberType().toString();
-            }
-        });
-        return null;
-    }
+//    @Override
+//    public Collection<GrantedAuthority> getAuthorities() {
+//        Collection<GrantedAuthority> collect = new ArrayList<>();
+//        // 권한(MemberType)을 리턴
+//        collect.add((GrantedAuthority) () -> member.getMemberType().toString());
+//        return null;
+//    }
 
     // User 의 password 리턴
     @Override
