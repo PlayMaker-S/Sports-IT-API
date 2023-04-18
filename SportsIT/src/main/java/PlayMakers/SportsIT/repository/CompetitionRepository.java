6bd6a63f9771dde25f8229f75627183eb9af8e32
MyeongQ <gmk0904@ajou.ac.kr>
2023-04-18 19:32:21 +0900

    [Feat] 대회 repository  추가 - 추후 기능 추가 예정

---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/repository/CompetitionRepository.java
@@ -0,0 +1,19 @@
+package PlayMakers.SportsIT.repository;
+
+import PlayMakers.SportsIT.domain.Competition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CompetitionRepository extends JpaRepository<Competition, Long> {
+    // 대회 아이디로 찾기
+    Competition findByCompetitionId(Long competitionId);
+    // 대회 이름으로 찾기
+    Optional<Competition> findByName(String name);
+    // 대회 상태로 찾기
+    Optional<Competition> findByState(String state);
+
+    // 대회 저장
+
+
+}
