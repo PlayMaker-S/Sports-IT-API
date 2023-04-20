6c68457522c4ac649d7a0590b2d9c28e5aeb4ff9
MyeongQ <gmk0904@ajou.ac.kr>
2023-04-20 18:19:23 +0900

    [Test] ã대회 관련 RepoFeat] 자동 설정 파일 추가

---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/AppConfig.java
@@ -0,0 +1,12 @@
+package PlayMakers.SportsIT;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(
+        basePackages = {"PlayMakers.SportsIT"}
+)
+public class AppConfig {
+
+}
