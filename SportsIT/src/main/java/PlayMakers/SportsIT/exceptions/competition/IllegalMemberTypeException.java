7ef0a9c68d686b4a43e3ed791a23b6704aef990e
MyeongQ <gmk0904@ajou.ac.kr>
2023-04-21 19:29:03 +0900

    [Feat] Error Message & Error Type 정의 : 대회필수정보누락, 대회정보유효성검사, 대회생성권한 확인

--- SportsIT/src/main/java/PlayMakers/SportsIT/exceptions/competition/IllegalMemberTypeException.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/exceptions/competition/IllegalMemberTypeException.java
@@ -1,2 +1,7 @@
-package PlayMakers.SportsIT.exceptions.competition;public class IllegalMemberTypeException {
+package PlayMakers.SportsIT.exceptions.competition;
+
+public class IllegalMemberTypeException extends IllegalArgumentException{
+    public IllegalMemberTypeException(String msg) {
+        super(msg);
+    }
 }
