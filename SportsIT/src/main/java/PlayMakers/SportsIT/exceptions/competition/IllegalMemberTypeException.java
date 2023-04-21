0eef228232a479a645dc28dde30f186c4ba80f3d
MyeongQ <gmk0904@ajou.ac.kr>
2023-04-21 19:27:19 +0900

    [Test] 대회 생성 단위테스트 성공

---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/exceptions/competition/IllegalMemberTypeException.java
@@ -0,0 +1,2 @@
+package PlayMakers.SportsIT.exceptions.competition;public class IllegalMemberTypeException {
+}
--- SportsIT/src/test/java/PlayMakers/SportsIT/competition/CompetitionServiceUnitTest.java
+++ SportsIT/src/test/java/PlayMakers/SportsIT/competition/CompetitionServiceUnitTest.java
@@ -14,9 +14,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.will;
+import static org.mockito.Mockito.when;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -39,26 +46,14 @@ class CompetitionServiceUnitTest {
             .roleName("ROLE_INSTITUTION")
             .build();
 
-    @BeforeEach
-    public void before() {
-        // 호스트 생성
-        Member host = Member.builder()
-                .pw("1234")
-                .name("홍길동")
-                .memberType(Collections.singleton(userTypeInst))
-                .email("host@gmail.com")
-                .phone("010-1234-5678")
-                .build();
-
-        System.out.println("host = " + host);
-        memberRepository.save(host);
-    }
     @Test
     @DisplayName("주최자는 대회를 생성할 수 있다.")
     public void 주최자대회생성() {
 
         // given 호스트가 대회 생성
-        Member host = memberRepository.findByEmail("host@gmail.com");
+        Long memberId = 1L;
+        //Member host = memberRepository.findByEmail("host@gmail.com");
+        Member host = Member.builder().uid(memberId).memberType(Collections.singleton(userTypeInst)).build();
 
         // host가 널일 경우
         CompetitionDto dto = CompetitionDto.builder()
@@ -68,16 +63,35 @@ class CompetitionServiceUnitTest {
                 .totalPrize(10000)
                 .content("대회내용")
                 .location("대회장소")
+                .locationDetail("대회장소상세")
                 .state(CompetitionState.RECRUITING)
+                .startDate(LocalDateTime.parse("2021-03-01T00:00:00"))
+                .recruitingStart(LocalDateTime.parse("2021-01-31T00:00:00"))
+                .recruitingEnd(LocalDateTime.parse("2021-02-28T00:00:00"))
+                .competitionType(CompetitionType.FREE)
+                .build();
+        Competition mockCompetition = Competition.builder()
+                .competitionId(1L)
+                .name("대회이름")
+                .host(host)
+                .category(SportCategory.ARM_WRESTLING)
+                .totalPrize(10000)
+                .content("대회내용")
+                .location("대회장소")
+                .locationDetail("대회장소상세")
+                .state(CompetitionState.RECRUITING)
+                .startDate(LocalDateTime.parse("2021-03-01T00:00:00"))
+                .recruitingStart(LocalDateTime.parse("2021-01-31T00:00:00"))
+                .recruitingEnd(LocalDateTime.parse("2021-02-28T00:00:00"))
+                .competitionType(CompetitionType.FREE)
                 .build();
 
         // when 대회 dto 생성
+        given(memberRepository.findById(memberId)).willReturn(Optional.ofNullable(host));
+        given(competitionRepository.save(any(Competition.class))).willReturn(Optional.of(mockCompetition).get());
         Competition created = competitionService.create(dto);
-        Competition saved = competitionRepository.findById(created.getCompetitionId()).get();
 
         // then
-        assertEquals(created.getCompetitionId(), saved.getCompetitionId());
-
-
+        assertNotNull(created);
     }
 }
