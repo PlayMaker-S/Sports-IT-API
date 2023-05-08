93d9004299272152394a2c215225167e21acdbc6
MyeongQ <gmk0904@ajou.ac.kr>
2023-05-08 17:06:33 +0900

    [Feat] findAllByIdUidAndCompetitionId 추가

--- SportsIT/src/main/java/PlayMakers/SportsIT/repository/JoinCompetitionRepository.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/repository/JoinCompetitionRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JoinCompetitionRepository extends JpaRepository<JoinCompetition, JoinCompetition.JoinCompetitionId> {
     List<JoinCompetition> findByIdUid(Long uid);
     List<JoinCompetition> findByIdCompetitionId(Long competitionId);
-    //Optional<JoinCompetition> findByUidAndCompetitionId(Long uid, Long competitionId);
+    Optional<JoinCompetition> findByIdUidAndIdCompetitionId(Long uid, Long competitionId);
     //Page<JoinCompetition> findByUid(Long uid, Pageable pageable);
     //Page<JoinCompetition> findByCompetitionId(Long competitionId, Pageable pageable);
     //void deleteByUidAndCompetitionId(Long uid, Long competitionId);
