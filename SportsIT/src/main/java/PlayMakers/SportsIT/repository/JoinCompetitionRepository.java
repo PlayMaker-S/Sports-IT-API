c4de0efdbada66502a65209e618070a49c66c2e4
MyeongQ <gmk0904@ajou.ac.kr>
2023-05-08 18:48:45 +0900

    [Test] JoinCompetition 삭제 Repo Unit test 성공

--- SportsIT/src/main/java/PlayMakers/SportsIT/repository/JoinCompetitionRepository.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/repository/JoinCompetitionRepository.java
@@ -14,7 +14,7 @@ public interface JoinCompetitionRepository extends JpaRepository<JoinCompetition
     Optional<JoinCompetition> findByIdUidAndIdCompetitionId(Long uid, Long competitionId);
     //Page<JoinCompetition> findByUid(Long uid, Pageable pageable);
     //Page<JoinCompetition> findByCompetitionId(Long competitionId, Pageable pageable);
-    //void deleteByUidAndCompetitionId(Long uid, Long competitionId);
+    void deleteByIdUidAndIdCompetitionId(Long uid, Long competitionId);
     //void deleteAllByCompetitionId(Long competitionId);
 
 }
--- SportsIT/src/test/java/PlayMakers/SportsIT/competition/JoinCompetitionRepositoryTest.java
+++ SportsIT/src/test/java/PlayMakers/SportsIT/competition/JoinCompetitionRepositoryTest.java
@@ -34,44 +34,7 @@ public class JoinCompetitionRepositoryTest {
     @Autowired
     JoinCompetitionRepository joinCompetitionRepository;
 
-    @BeforeEach
-    public void before() {
-        joinCompetitionRepository.deleteAll();
-        competitionRepository.deleteAll();
-        memberRepository.deleteAll();
-    }
-
-    @Test
-    @DisplayName("대회 신청")
-    public void 대회신청() {
-        // given
-        Member host = Member.builder()
-                .email("host@email.com")
-                .name("host")
-                .memberType(Collections.singleton(MemberType.builder().roleName("ROLE_INSTITUTION").build()))
-                .pw("1234")
-                .phone("010-1234-5678")
-                .build();
-        memberRepository.save(host);
-
-        Member player = Member.builder()
-                .email("player@email.com")
-                .name("player")
-                .memberType(Collections.singleton(MemberType.builder().roleName("ROLE_USER").build()))
-                .pw("1234")
-                .phone("010-1111-1111")
-                .build();
-        memberRepository.save(player);
-
-        Member viewer = Member.builder()
-                .email("viewer@email.com")
-                .name("viewer")
-                .memberType(Collections.singleton(MemberType.builder().roleName("ROLE_USER").build()))
-                .pw("1234")
-                .phone("010-2222-2222")
-                .build();
-        memberRepository.save(viewer);
-
+    private static Competition createCompetition(Member host) {
         Competition competition = Competition.builder()
                 .host(host)
                 .category(SportCategory.SOCCER)
@@ -85,6 +48,63 @@ public class JoinCompetitionRepositoryTest {
                 .startDate(LocalDateTime.now().plusDays(5))
                 .endDate(LocalDateTime.now().plusDays(7))
                 .build();
+        return competition;
+    }
+
+    private static Member createViewer(int idx) {
+        Member viewer = Member.builder()
+                .email("viewer"+idx+"@email.com")
+                .name("viewer")
+                .memberType(Collections.singleton(MemberType.builder().roleName("ROLE_USER").build()))
+                .pw("1234")
+                .phone("010-2222-222"+idx)
+                .build();
+        return viewer;
+    }
+
+    private static Member createPlayer(int idx) {
+        Member player = Member.builder()
+                .email("player"+idx+"@email.com")
+                .name("player")
+                .memberType(Collections.singleton(MemberType.builder().roleName("ROLE_USER").build()))
+                .pw("1234")
+                .phone("010-1111-111"+idx)
+                .build();
+        return player;
+    }
+
+    private static Member createHost(int idx) {
+        Member host = Member.builder()
+                .email("host" + idx + "@email.com")
+                .name("host")
+                .memberType(Collections.singleton(MemberType.builder().roleName("ROLE_INSTITUTION").build()))
+                .pw("1234")
+                .phone("010-1234-567"+idx)
+                .build();
+        return host;
+    }
+
+    @BeforeEach
+    public void before() {
+        joinCompetitionRepository.deleteAll();
+        competitionRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("대회 신청")
+    public void 대회신청() {
+        // given
+        Member host = createHost(1);
+        memberRepository.save(host);
+
+        Member player = createPlayer(1);
+        memberRepository.save(player);
+
+        Member viewer = createViewer(1);
+        memberRepository.save(viewer);
+
+        Competition competition = createCompetition(host);
         competitionRepository.save(competition);
 
         // when
@@ -127,6 +147,48 @@ public class JoinCompetitionRepositoryTest {
         Assert.assertEquals(joinAsViewer.getId().getUid(), joinCompetitionRepository.findByIdUidAndIdCompetitionId(viewer.getUid(), competition.getCompetitionId()).get().getId().getUid());
     }
 
+    @Test
+    @DisplayName("단일 대회 신청 취소")
+    public void 단일대회신청취소() {
+        // given
+        Member host = createHost(1);
+        memberRepository.save(host);
 
+        Member player = createPlayer(1);
+        memberRepository.save(player);
+
+        Member viewer = createViewer(1);
+        memberRepository.save(viewer);
+
+        Competition competition = createCompetition(host);
+        competitionRepository.save(competition);
+
+        JoinCompetition joinAsPlayer = JoinCompetition.builder()
+                .competition(competition)
+                .member(player)
+                .id(new JoinCompetition.JoinCompetitionId(player.getUid(), competition.getCompetitionId()))
+                .formId("1")
+                .isAgree(true)
+                .isPaid(true)
+                .joinType(JoinCompetition.joinType.PLAYER)
+                .build();
+
+        JoinCompetition joinAsViewer = JoinCompetition.builder()
+                .competition(competition)
+                .member(viewer)
+                .id(new JoinCompetition.JoinCompetitionId(viewer.getUid(), competition.getCompetitionId()))
+                .formId("2")
+                .isAgree(true)
+                .isPaid(true)
+                .joinType(JoinCompetition.joinType.VIEWER)
+                .build();
+
+        joinCompetitionRepository.save(joinAsPlayer);
+        joinCompetitionRepository.save(joinAsViewer);
+
+        // when
+        joinCompetitionRepository.deleteByIdUidAndIdCompetitionId(player.getUid(), competition.getCompetitionId());
+
+    }
 
 }
