bc9151cd8a8aacedfc697ee20b6d62e53aecd725
MyeongQ <gmk0904@ajou.ac.kr>
2023-05-15 14:19:46 +0900

    [Feta] 다중 multipart file upload 기능 추가

--- SportsIT/src/main/java/PlayMakers/SportsIT/service/S3Uploader.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/service/S3Uploader.java
@@ -13,11 +13,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -37,12 +41,22 @@ public class S3Uploader {
     // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
         File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+                .orElseThrow(() -> new MultipartException("MultipartFile -> File 전환 실패"));
         return upload(uploadFile, dirName);
     }
 
+    public List<String> uploadImages(List<MultipartFile> multipartFiles, String dirName) throws IOException {
+        List<String> savedUrls = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            File uploadFile = convert(multipartFile)
+                    .orElseThrow(() -> new MultipartException("MultipartFile -> File 전환 실패"));
+            savedUrls.add(upload(uploadFile, dirName));
+        }
+        return savedUrls;
+    }
+
     private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + uploadFile.getName();
+        String fileName = dirName + "/" + LocalDateTime.now() + "_" + uploadFile.getName();    // S3에 저장된 파일 이름
         String uploadImageUrl = putS3(uploadFile, fileName);
 
         removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
