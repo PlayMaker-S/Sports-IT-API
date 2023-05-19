ce21ce5c95705a09b36d7a80b0972f4f0c0545bd
MyeongQ <gmk0904@ajou.ac.kr>
2023-05-19 15:08:41 +0900

    [Feat] 대회 규정 등록 로직 구현

---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/repository/AgreementRepository.java
@@ -0,0 +1,15 @@
+package PlayMakers.SportsIT.repository;
+
+import PlayMakers.SportsIT.domain.Agreement;
+import PlayMakers.SportsIT.domain.Poster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AgreementRepository extends JpaRepository<Agreement, Long> {
+    Optional<Agreement> findByAgreementUrl(String url);
+    List<Agreement> findByCompetitionCompetitionId(Long id);
+}
