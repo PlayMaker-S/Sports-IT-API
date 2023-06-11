29e981ba8d3587e3c484bf8db9e1859602f2859e
MyeongQ <gmk0904@ajou.ac.kr>
2023-06-11 21:34:35 +0900

    [Feat] FeelIt Controller 생성

---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/controller/FeelItController.java
@@ -0,0 +1,11 @@
+package PlayMakers.SportsIT.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FeelItController {
+    private final
+}
