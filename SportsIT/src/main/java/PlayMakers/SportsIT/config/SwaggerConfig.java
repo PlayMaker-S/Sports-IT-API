4054026a753104e97915f9b3c9fb44d0973ecc6b
MyeongQ <gmk0904@ajou.ac.kr>
2023-08-24 18:37:53 +0900

    [Feat] 메인 Description, 대회 Description 수정

--- SportsIT/src/main/java/PlayMakers/SportsIT/config/SwaggerConfig.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/config/SwaggerConfig.java
@@ -12,8 +12,8 @@ import java.sql.Array;
 import java.util.Arrays;
 
 @OpenAPIDefinition(
-        info = @Info(title = "스포츠-잇 API 명세서",
-                description = "API 명세 및 스키마를 확인하고 API 테스트 호출 가능합니다.",
+        info = @Info(title = "스포츠잇 API 명세서",
+                description = "API 명세 및 데이터 스키마를 확인하고 API 테스트 호출 가능합니다.",
                 version = "0.0.1"),
         servers = {
                 @Server(url = "https://localhost:8443", description = "Local server"),
