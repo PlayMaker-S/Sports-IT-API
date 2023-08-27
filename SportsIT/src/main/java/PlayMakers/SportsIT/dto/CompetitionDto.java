7760fce59f2a4937346323ea37322fd20c7dbec5
MyeongQ <gmk0904@ajou.ac.kr>
2023-08-27 16:41:50 +0900

    [Feat] MemberDto, CompetitionDto, LoginDto 스키마 추가

--- SportsIT/src/main/java/PlayMakers/SportsIT/dto/CompetitionDto.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/dto/CompetitionDto.java
@@ -2,6 +2,7 @@ package PlayMakers.SportsIT.dto;
 
 import PlayMakers.SportsIT.domain.*;
 import PlayMakers.SportsIT.enums.CompetitionType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,38 +14,65 @@ import java.util.List;
 @Data // @ToString, @EqualsAndHashCode, @Getter, @Setter, @RequiredArgsConstructor
 public class CompetitionDto {
 
+    @Schema(description = "대회 ID", example = "1")
     private Long competitionId;
+    @Schema(description = "대회 이름", example = "스포츠잇 팔씨름 대회")
     private String name;
+    @Schema(description = "대회 주최자 객체")
     private Member host;
+    @Schema(description = "대회 종목 코드 리스트 / 현재는 대회 생성시 필요 X", example = "{\"ARM_WRESTLING\"}")
     private List<String> categories;
+    @Schema(description = "종목 코드", example = "ARM_WRESTLING")
     private SportCategory sportCategory;
+    @Schema(description = "조회수", example = "30")
     @Builder.Default
     private Integer viewCount = 0;
+    @Schema(description = "스크랩수", example = "10")
     @Builder.Default
     private Integer scrapCount = 0;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    @Schema(description = "모집 시작일", example = "2021-09-15T00:00:00")
     private LocalDateTime recruitingStart;
+    @Schema(description = "모집 종료일", example = "2021-09-25T00:00:00")
     private LocalDateTime recruitingEnd;
+    @Schema(description = "대회 시작일", example = "2023-09-30T00:00:00")
+    private LocalDateTime startDate;
+    @Schema(description = "대회 종료일", example = "2021-10-05T00:00:00")
+    private LocalDateTime endDate;
+    @Schema(description = "총 상금", example = "1000000")
     private Integer totalPrize;
+    @Schema(description = "대회 상세 정보", example = "스포츠잇 팔씨름 대회를 개최합니다.")
     private String content;
+    @Schema(description = "개최 주소1", example = "서울특별시 강남구")
     private String location;
+    @Schema(description = "개최 주소2", example = "테헤란로 427")
     private String locationDetail;
+    @Schema(description = "개최 주소 위도", example = "37.5665")
     private Double latitude;
+    @Schema(description = "개최 주소 경도", example = "126.9780")
     private Double longitude;
+    @Schema(description = "대회 상태 / 대회 생성시 필요 X", example = "RECRUITING")
     private CompetitionState state;
+    @Schema(description = "대회 취소 사유 / 대회 생성시 필요 X", example = "우천으로 인해 불가피하게 대회를 취소하게 되었습니다.")
     private String stateDetail;
+    @Schema(description = "대회 타입 / 대회 생성시 필요 X", example = "BASIC")
     private CompetitionType competitionType;
+    @Schema(description = "대회 설문지 템플릿 ID / 대회 생성시 필요 X", example = "14")
     private String templateId;
+    @Schema(description = "최대 참가자 수", example = "999")
     @Builder.Default
     private Integer maxPlayer = 999;
+    @Schema(description = "최대 관람자 수", example = "999")
     @Builder.Default
     private Integer maxViewer = 999;
+    @Schema(description = "대회 포스터 리스트")
     @Builder.Default
     private List<Poster> posters = new ArrayList<>();
+    @Schema(description = "대회 규약 리스트")
     @Builder.Default
     private List<Agreement> agreements = new ArrayList<>();
+    @Schema(description = "대회 게시물 생성일 / 대회 생성시 필요 X", example = "2021-09-15T00:00:00")
     private String createdDate;
+    @Schema(description = "대회 게시물 수정일 / 대회 생성시 필요 X", example = "2021-09-15T00:00:00")
     private String updatedDate;
     private List<String> category;
 
--- SportsIT/src/main/java/PlayMakers/SportsIT/dto/MemberDto.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package PlayMakers.SportsIT.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,14 +15,23 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class MemberDto {
+    @Schema(description = "사용자 아이디, 회원가입시 필요 X", example = "sportsit_admin2@abc.com")
     private String loginId;
+    @Schema(description = "비밀번호", example = "1234")
     private String pw;
+    @Schema(description = "사용자 이름", example = "스포츠잇 관리자")
     private String name;
+    @Schema(description = "사용자 이메일", example = "sportsit_admin2@abc.com")
     private String email;
+    @Schema(description = "전화번호", example = "010-9797-7979")
     private String phone;
+    @Schema(description = "사용자 소개, 회원가입시 필요 없음", example = "스포츠잇 관리자입니다.")
     private String description;
+    @Schema(description = "활성계정 여부, 회원가입시 자동으로 true 설정", example = "true")
     private boolean activated;
+    @Schema(description = "회원 타입, 체육인/주최자", example = "ROLE_PLAYER")
     private String memberType;
+    @Schema(description = "관심 종목", example = "[\"SOCCER\", \"HOCKEY\", \"RUNNING\"]")
     private List<String> categories;
 
     @Data
