2365ee3ec89a4907f131466babc9d9d36ba2db36
MyeongQ <gmk0904@ajou.ac.kr>
2023-09-08 13:59:29 +0900

    [Feat] JoinCount Schema 추가 및 SchemaExample 분리

--- SportsIT/src/main/java/PlayMakers/SportsIT/config/SwaggerConfig.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/config/SwaggerConfig.java
@@ -1,12 +1,11 @@
 package PlayMakers.SportsIT.config;
 
+import PlayMakers.SportsIT.utils.swagger.SchemaExample;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.media.*;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import lombok.RequiredArgsConstructor;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +14,8 @@ import org.springframework.context.annotation.Configuration;
 import java.sql.Array;
 import java.util.Arrays;
 import java.util.Map;
+
+import static PlayMakers.SportsIT.utils.swagger.SchemaExample.*;
 
 @OpenAPIDefinition(
         info = @Info(title = "스포츠잇 API 명세서",
@@ -26,39 +27,6 @@ import java.util.Map;
         })
 @Configuration
 public class SwaggerConfig {
-
-    // Schemas
-    Schema PostResponse = new Schema<Map<String, Object>>()
-            .addProperty("timestamp",new StringSchema().example("2023-08-30T13:17:04.846Z"))
-            .addProperty("code",new StringSchema().example("201"))
-            .addProperty("success",new StringSchema().example("true"))
-            .addProperty("result",new StringSchema().example("{}"));
-
-
-    Schema CompetitionForm = new Schema<Map<String, Object>> ()
-        .addProperty("timestamp",new StringSchema().example("2023-08-30T13:17:04.846Z"))
-        .addProperty("code",new StringSchema().example("200"))
-        .addProperty("success",new StringSchema().example("true"))
-        .addProperty("result",new JsonSchema()
-            .addProperty("name", new StringSchema().example("스포츠잇 팔씨름 대회"))
-            .addProperty("categories", new ArraySchema().items(new StringSchema().example("ARM_WRESTLING")))
-            .addProperty("sportCategory", new StringSchema().example("ARM_WRESTLING"))
-            .addProperty("recruitingStart", new StringSchema().example("2023-09-15T00:00:00Z"))
-            .addProperty("recruitingEnd", new StringSchema().example("2023-09-20T00:00:00Z"))
-            .addProperty("startDate", new StringSchema().example("2023-09-30T00:00:00Z"))
-            .addProperty("endDate", new StringSchema().example("2023-10-05T00:00:00Z"))
-            .addProperty("totalPrize", new IntegerSchema().example(1000000))
-            .addProperty("content", new StringSchema().example("스포츠잇 팔씨름 대회를 개최합니다."))
-            .addProperty("location", new StringSchema().example("서울특별시 강남구 테헤란로 626"))
-            .addProperty("locationDetail", new StringSchema().example("메디톡스빌딩, 2층"))
-            .addProperty("latitude", new NumberSchema().example(37.5665))
-            .addProperty("longitude", new NumberSchema().example(126.978))
-            .addProperty("templateId", new StringSchema().example("x8mn9Bwj8u4Sm5c69rUV"))
-            .addProperty("maxPlayer", new IntegerSchema().example(300))
-            .addProperty("maxViewer", new IntegerSchema().example(999)));
-
-
-
 
 
     @Bean
@@ -96,6 +64,7 @@ public class SwaggerConfig {
 
     public OpenApiCustomizer registerSchemas() {
         return openApi -> openApi.getComponents()
+                .addSchemas("JoinCount", JoinCount)
                 .addSchemas("PostResponse", PostResponse)
                 .addSchemas("CompetitionForm", CompetitionForm);
     }
--- SportsIT/src/main/java/PlayMakers/SportsIT/utils/swagger/SchemaExample.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/utils/swagger/SchemaExample.java
@@ -1,2 +1,44 @@
-package PlayMakers.SportsIT.utils.swagger;public class examples {
+package PlayMakers.SportsIT.utils.swagger;
+
+import io.swagger.v3.oas.models.media.*;
+
+import java.util.Map;
+
+public class SchemaExample {
+    public static Schema PostResponse = new Schema<Map<String, Object>>()
+            .addProperty("timestamp",new StringSchema().example("2023-08-30T13:17:04.846Z"))
+            .addProperty("code",new StringSchema().example("201"))
+            .addProperty("success",new StringSchema().example("true"))
+            .addProperty("result",new StringSchema().example("{}"));
+
+
+    public static Schema CompetitionForm = new Schema<Map<String, Object>> ()
+            .addProperty("timestamp",new StringSchema().example("2023-08-30T13:17:04.846Z"))
+            .addProperty("code",new StringSchema().example("200"))
+            .addProperty("success",new StringSchema().example("true"))
+            .addProperty("result",new JsonSchema()
+                    .addProperty("name", new StringSchema().example("스포츠잇 팔씨름 대회"))
+                    .addProperty("categories", new ArraySchema().items(new StringSchema().example("ARM_WRESTLING")))
+                    .addProperty("sportCategory", new StringSchema().example("ARM_WRESTLING"))
+                    .addProperty("recruitingStart", new StringSchema().example("2023-09-15T00:00:00Z"))
+                    .addProperty("recruitingEnd", new StringSchema().example("2023-09-20T00:00:00Z"))
+                    .addProperty("startDate", new StringSchema().example("2023-09-30T00:00:00Z"))
+                    .addProperty("endDate", new StringSchema().example("2023-10-05T00:00:00Z"))
+                    .addProperty("totalPrize", new IntegerSchema().example(1000000))
+                    .addProperty("content", new StringSchema().example("스포츠잇 팔씨름 대회를 개최합니다."))
+                    .addProperty("location", new StringSchema().example("서울특별시 강남구 테헤란로 626"))
+                    .addProperty("locationDetail", new StringSchema().example("메디톡스빌딩, 2층"))
+                    .addProperty("latitude", new NumberSchema().example(37.5665))
+                    .addProperty("longitude", new NumberSchema().example(126.978))
+                    .addProperty("templateId", new StringSchema().example("x8mn9Bwj8u4Sm5c69rUV"))
+                    .addProperty("maxPlayer", new IntegerSchema().example(300))
+                    .addProperty("maxViewer", new IntegerSchema().example(999)));
+
+    public static Schema JoinCount = new Schema<Map<String, Object>>()
+            .addProperty("timestamp",new StringSchema().example("2023-08-30T13:17:04.846Z"))
+            .addProperty("code",new StringSchema().example("200"))
+            .addProperty("success",new StringSchema().example("true"))
+            .addProperty("result",new JsonSchema()
+            .addProperty("availablePlayer", new StringSchema().example("300"))
+            .addProperty("availableViewer", new StringSchema().example("999")));
 }
