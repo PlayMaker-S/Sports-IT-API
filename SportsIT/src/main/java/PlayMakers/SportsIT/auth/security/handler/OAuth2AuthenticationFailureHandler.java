8209cd85d142619fa60ee02a68c453f6e7f2b5f4
MyeongQ <gmk0904@ajou.ac.kr>
2023-10-12 23:07:35 +0900

    [Feat] OAuth 성공/실패 로직 등록

---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/auth/security/handler/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,39 @@
+package PlayMakers.SportsIT.auth.security.handler;
+
+import PlayMakers.SportsIT.auth.security.cookie.CookieAuthorizationRequestRepository;
+import PlayMakers.SportsIT.auth.security.cookie.CookieUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+
+import static PlayMakers.SportsIT.auth.security.cookie.CookieAuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException {
+        String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse("/");
+
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("error", authenticationException.getLocalizedMessage())
+                .build().toUriString();
+
+        cookieAuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+}
---
+++ SportsIT/src/main/java/PlayMakers/SportsIT/auth/security/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,78 @@
+package PlayMakers.SportsIT.auth.security.handler;
+
+import PlayMakers.SportsIT.auth.security.cookie.CookieAuthorizationRequestRepository;
+import PlayMakers.SportsIT.auth.security.cookie.CookieUtils;
+import PlayMakers.SportsIT.auth.security.jwt.JwtTokenProvider;
+import PlayMakers.SportsIT.dto.UserResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
+import static PlayMakers.SportsIT.auth.security.cookie.CookieAuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Value("${oauth.authorizedRedirectUri}")
+    private String redirectUri;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(request, response, authentication);
+
+        if (response.isCommitted()) {
+            log.debug("Response has already been committed.");
+            return;
+        }
+        clearAuthenticationAttributes(request, response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue);
+
+        if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+            throw new RuntimeException("redirect URIs are not matched.");
+        }
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+        //JWT 생성
+        UserResponseDto.TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("token", tokenInfo.getAccessToken())
+                .build().toUriString();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        cookieAuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    private boolean isAuthorizedRedirectUri(String uri) {
+        URI clientRedirectUri = URI.create(uri);
+        URI authorizedUri = URI.create(redirectUri);
+
+        if (authorizedUri.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+                && authorizedUri.getPort() == clientRedirectUri.getPort()) {
+            return true;
+        }
+        return false;
+    }
+}
