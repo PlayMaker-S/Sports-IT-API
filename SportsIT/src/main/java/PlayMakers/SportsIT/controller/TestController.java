1ad42631598891d6be08689f95d3288a95e33891
MyeongQ <gmk0904@ajou.ac.kr>
2023-10-21 00:52:02 +0900

    [Fix] TestController.java 이름 변경

--- SportsIT/src/main/java/PlayMakers/SportsIT/controller/TestController.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/controller/TestController.java
@@ -6,34 +6,46 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @Controller
-public class testController {
+public class TestController {
     @RequestMapping("/test")
     public String test(){
         return "payment";
     }
 
     // Json을 반환하는 컨트롤러
+//    @RequestMapping("/oauth")
+//    public ResponseEntity<CommonResponse> oauth(@PathVariable String state, @PathVariable String code, @PathVariable String scope){
+//
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("state", state);
+//        data.put("code", code);
+//        data.put("scope", scope);
+//
+//        Map<String, Object> result = new HashMap<>();
+//        result.put("data", data);
+//        result.put("message", "success");
+//        log.info("state: {}  code: {}  scope: {}", state, code, scope);
+//        return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK.value(), result));
+//    }
+
     @RequestMapping("/oauth")
-    public ResponseEntity<CommonResponse> oauth(@PathVariable String state, @PathVariable String code, @PathVariable String scope){
+    public ResponseEntity<CommonResponse> oauth(@RequestParam String token){
 
         Map<String, Object> data = new HashMap<>();
-        data.put("state", state);
-        data.put("code", code);
-        data.put("scope", scope);
+        data.put("token", token);
 
         Map<String, Object> result = new HashMap<>();
         result.put("data", data);
         result.put("message", "success");
-        log.info("state: {}  code: {}  scope: {}", state, code, scope);
+        log.info("token: {}", token);
         return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK.value(), result));
     }
 
