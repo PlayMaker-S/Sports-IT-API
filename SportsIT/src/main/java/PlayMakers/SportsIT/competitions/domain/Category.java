d91ada6df59adfc99b9a7f58465108c6c6783261
MyeongQ <gmk0904@ajou.ac.kr>
2024-01-09 19:30:19 +0900

    [Feat] 대회 종목 및 대분류 종목 추가

--- SportsIT/src/main/java/PlayMakers/SportsIT/competitions/domain/Category.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/competitions/domain/Category.java
@@ -1,24 +1,41 @@
 package PlayMakers.SportsIT.competitions.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor @NoArgsConstructor
 @Getter
 @Builder
 public class Category {
-    @Id @Column(length = 30)
-    private String category;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
 
     @Column(length = 30)
     private String name;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_code")
+    private CategoryGroup categoryGroup;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    },
+            mappedBy = "categories")
+    @JsonIgnore
+    private Set<Competition> competitions = new HashSet<>();
+
+    // hashCode
+    @Override
+    public int hashCode() {
+        return code.hashCode() + categoryGroup.getCode().hashCode();
+    }
 }
--- SportsIT/src/main/java/PlayMakers/SportsIT/competitions/domain/CategoryGroup.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/competitions/domain/CategoryGroup.java
@@ -1,2 +1,22 @@
-package PlayMakers.SportsIT.competitions.domain;public class CategoryGroup {
+package PlayMakers.SportsIT.competitions.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cat_group")
+@Builder
+@Getter
+@NoArgsConstructor @AllArgsConstructor
+public class CategoryGroup {
+    @Id
+    private Long code;
+    @Column(length = 30)
+    private String name;
 }
