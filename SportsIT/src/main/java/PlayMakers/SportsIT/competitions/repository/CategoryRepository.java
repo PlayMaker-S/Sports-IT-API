89611413db32110015d2dee99d4b6aef4f343405
MyeongQ <gmk0904@ajou.ac.kr>
2024-01-09 19:44:13 +0900

    [Feat] CategoryRepo 추가

--- SportsIT/src/main/java/PlayMakers/SportsIT/competitions/repository/CategoryRepository.java
+++ SportsIT/src/main/java/PlayMakers/SportsIT/competitions/repository/CategoryRepository.java
@@ -1,4 +1,4 @@
-package PlayMakers.SportsIT.repository;
+package PlayMakers.SportsIT.competitions.repository;
 
 import PlayMakers.SportsIT.competitions.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, String> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
+    Optional<Category> findByCode(Long code);
 }
